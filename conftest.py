f9306b97d212f0be841d0c72f77e931ea715492f
Rishat <rishatbmstu@gmail.com>
2023-07-03 21:40:40 +0300

    dont works

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+# options.add_argument("--window-size=1920,800")
 
 @pytest.fixture()
 def set_up():
@@ -11,7 +12,7 @@ def set_up():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,800")
+
     options.add_argument("--no-sandbox")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
