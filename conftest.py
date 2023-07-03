17dcf5b33a2f02c3e342145106915910efe13b3d
Rishat <rishatbmstu@gmail.com>
2023-07-03 13:09:25 +0300

    window, head

--- conftest.py
+++ conftest.py
@@ -9,10 +9,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def set_up():
     print("START TEST")
     options = Options()
-    options.add_argument("--window-size=1920,800")
-    options.add_argument("--headless")
+    # options.add_argument("--window-size=1920,800")
+    # options.add_argument("--headless")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) #, options=options)
     url = "https://www.wildberries.ru/"
     driver.get(url)
     driver.maximize_window()
