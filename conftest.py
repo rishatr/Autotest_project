d3d9d333f74e9cf277ff4a72be4c6f03029cabb3
Rishat <rishatbmstu@gmail.com>
2023-07-03 21:17:31 +0300

    head

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def set_up():
     print("START TEST")
     options = Options()
-    options.add_argument("--headless")
+# options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,800")
     options.add_argument("--no-sandbox")
